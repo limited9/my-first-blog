9d622f78d3895c8feabb40cfddd47a0de3378c9d
jw <test@test.com>
2017-02-01 03:51:23 +0900

    Added views to create/edit blog post inside the site.

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Post
+
+class  PostForm(forms.ModelForm):
+
+    class Meta: # 폼을 만들기 위해 어떤 모델이 쓰여야 하는지 장고에게 알려주는 구문
+        model = Post
+        fields = ('title', 'text', ) # title과 text 컬럼
--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'), #문자열이 아무것도 없는 경우 매칭
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
