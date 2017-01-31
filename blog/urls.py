755bfce0080dc4d64c7bae7396d11fb17d25b5b8
jw <test@test.com>
2017-01-31 10:01:44 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'), #문자열이 아무것도 없는 경우 매칭
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
