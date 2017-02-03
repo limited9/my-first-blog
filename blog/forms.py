728c31743d4b4480ef8d9942e39071bc65ff0d83
jw <test@test.com>
2017-02-04 05:10:47 +0900

    comment.

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 class  PostForm(forms.ModelForm):
 
     class Meta: # 폼을 만들기 위해 어떤 모델이 쓰여야 하는지 장고에게 알려주는 구문
         model = Post
         fields = ('title', 'text', ) # title과 text 컬럼
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text')
